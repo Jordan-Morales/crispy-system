4b4844fed79c090964ac7a6607beabf69aa6e3e0
Jordan Morales <jordan.morales@gmail.com>
2019-11-05 09:11:13 -0800

    setup working

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-//--------------------
-//
-//--------------------
+
+app.listen(PORT, () => {
+  console.log("listening on port: ", PORT);
+})
