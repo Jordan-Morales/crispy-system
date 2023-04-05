37b31e96259d87cc8078eeaeff629603127e98ff
Jordan Morales <52443263+Jordan-Morales@users.noreply.github.com>
2023-04-05 01:59:22 -0500

    updated ejs
    refactored ejs and partials for updated structure.

--- server.js
+++ server.js
@@ -12,29 +12,29 @@ require('dotenv').config();
 //--------------------
 // PORT
 //--------------------
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 console.log(PORT);
 
 //--------------------
 // DATABASE
 //--------------------
-const MONGODB_URI= process.env.MONGODB_URI
-mongoose.connect( MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+const MONGODB_URI= process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected'))
+db.on('connected', () => console.log('mongo connected'));
 // ; MONGODB_URI, '||||', CLOUDINARY_URL
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 //--------------------
 // Cloudinary
 //--------------------
-const CLOUDINARY_URL = process.env.CLOUDINARY_URL
+const CLOUDINARY_URL = process.env.CLOUDINARY_URL;
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
 });
 
 // Server-side Image Uploader //
@@ -47,15 +47,15 @@ cloudinary.config({
 // open public folder
 app.use(express.static('public'));
 //urlencoding false
-app.use(express.urlencoded( {extended: false} ));
+app.use(express.urlencoded({extended: false}));
 // opens method override function
 app.use(methodOverride('_method'));
 // open session
 app.use(session({
-  secret:'O6sLoDV18O',
-  resave: false,
-  saveUninitialized: false
-}))
+    secret: 'O6sLoDV18O',
+    resave: false,
+    saveUninitialized: false
+}));
 
 //--------------------
 //Routes
@@ -77,23 +77,26 @@ app.use(brewsController);
 
 //main loading page containing welcome message and links to login / signup
 app.get('/', (req, res) => {
-  res.render('home.ejs')
+    'use strict';
+    res.render('home.ejs');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.ejs')
+    'use strict';
+    res.render('about.ejs');
 });
 
 // Logout
 app.delete('/destroy', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect('/')
-    }
-  })
-})
+    'use strict';
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+});
 //
 
 
@@ -101,5 +104,6 @@ app.delete('/destroy', (req, res) => {
 //Listener
 //--------------------
 app.listen(PORT, () => {
-  console.log("listening on port: ", PORT);
-})
+    'use strict';
+    console.log("listening on port: ", PORT);
+});
